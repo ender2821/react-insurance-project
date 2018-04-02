6adaa294ac66856974e3af106c7963cc4be621d4
Joshua Jensen <ender2821@yahoo.com>
2018-04-02 17:15:41 -0500

    added functionality to form fields and added elements for groups

--- insurance-project/src/Components/FieldDetails.js
+++ insurance-project/src/Components/FieldDetails.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ButtonBar from './ButtonBar';
 
 class FieldDetails extends React.Component{
-    
+    state = {
+        referenceName: ''
+    }
+    removeSpaces = (event) => {
+        const value = event.target.value;
+        const trim = value.replace(/\s/g, "");
+        this.setState({ referenceName: trim})
+    }
+    checkRegex = (event) => {
+        const value = event.target.value;
+        const str = 'test sentance';
+        const regex = str.replace(value, "");
+        console.log(str);
+        console.log(regex);
+        if (str === regex){
+            console.log('FAILED');
+        }
+    }
+
     render(){
         return (
             <div className='right-container'>
@@ -11,12 +29,13 @@ class FieldDetails extends React.Component{
                         <h2>Field Details</h2>
                         <form>
                             <label>Display Label</label>
-                            <input type="text" name='Display Label' />
+                            <input type='text' name='Display Label' onKeyUp={this.removeSpaces} />
+
                             <span>For display purposes, spaces allowed.</span>
                         </form>
                         <form>
                             <label>Reference Name</label>
-                            <input type="text" name='Display Label' />
+                            <input type="text" name='Display Label' disabled value={this.state.referenceName}/>
                             <span>Used to reference in calculations, no spaces allowed</span>
                         </form>                        
                         <form>
@@ -25,7 +44,7 @@ class FieldDetails extends React.Component{
                         </form>
                         <form>
                             <label>Custom Validation</label>
-                            <input type='text' name='Custom Validation' />
+                            <input type='text' name='Custom Validation' onKeyUp={this.checkRegex}/>
                             <span>Any regex patter can be used for custom input validation</span>
                         </form>
                         <form>
@@ -47,6 +66,10 @@ class FieldDetails extends React.Component{
                     <div className='col-3'>
                         <h2>Field Groups</h2>
                         <h4>Choose a group for this input</h4>
+                        <button className="btn secondary">+ Add a Group</button>
+                        <ul className='group-container'>
+                            <li className='group-button'>Example Group</li>
+                        </ul>
                     </div>                 
                 </div>
                 <ButtonBar unloadFieldDetails={this.props.unloadFieldDetails}/>
